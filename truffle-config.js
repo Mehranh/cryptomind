148c6c74c7bc3f70ffa2111c490797b8e0d24ed1
Mehran <mehran.hafizi@gmail.com>
2020-09-13 00:16:20 +0430

    change compiler

--- truffle-config.js
+++ truffle-config.js
@@ -5,19 +5,26 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //}
-  //
+ networks: {
+  development: {
+    host: "127.0.0.1",
+    port: 7545,
+    network_id: "*"
+  },
+  test: {
+    host: "127.0.0.1",
+    port: 7545,
+    network_id: "*"
+  },
+   "ropsten-infura": {
+        provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + infuraKey, 0),
+        network_id: 3,
+        gas: 5500000,
+        gasPrice: 100000000000,
+        timeoutBlocks: 200,
+    }
+}
+
 
  compilers: {
      solc: {
